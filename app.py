9d8b9f6de16aac51a213e84e793ba08071fbc80e
Aikanshi Vaish <49493050+ekanshivaish@users.noreply.github.com>
2021-09-22 16:29:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,62 +1,75 @@
-from flask import Flask, render_template, url_for, request
 import pandas as pd
+import numpy as np
+import streamlit as st
+import nltk
+import regex as re
+import sklearn
 import pickle
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.externals import joblib
-import pickle
+lm = WordNetLemmatizer()
+sw = stopwords.words('english')
 
-# load the model from disk
-filename = 'final_model_02082021.pkl'
-clf = pickle.load(open(filename, 'rb'))
-cv = pickle.load(open('cv_vec_02082021.pkl', 'rb'))
-app = Flask(__name__)
+model = pd.read_pickle(r'C:\Users\HP\PycharmProjects\pythonProject\final_model_02082021.pkl')
+vect = pd.read_pickle(r'C:\Users\HP\PycharmProjects\pythonProject\cv_vec_02082021.pkl')
 
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+def clean_data(value):
+    value = value.lower()
+    value = re.sub(r'[^\w\s]', '', value)
+    value = re.sub(r'[^a-zA-Z]', ' ', value)
+    value = re.sub('aa[a]*', 'a', value)
+    value = re.sub(r"http\S+", "", value)
+    value = re.sub(r'\<a href', ' ', value)
+    value = re.sub(r'[_"\;%()|+&=*%.,!?:#$@\[\]/]', ' ', value)
+    value = re.sub('-', ' ', value)
+    value = re.sub('\d+', '', value)
+    value = re.sub('\s\s+', ' ', value)
+    value = re.sub('\n', '', value)
+    value = re.sub('<[^>]*>', '', value)
+    return value
 
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    #df= pd.read_csv("spam.csv", encoding="latin-1")
-   # df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-    #	# Features and Labels
-    #df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-    #X = df['message']
-    #y = df['label']
-    #
-    #	# Extract Feature With CountVectorizer
-    #cv = CountVectorizer()
-    #X = cv.fit_transform(X) # Fit the Data
-    #
-    #pickle.dump(cv, open('cv_vec_02082021.pkl', 'wb'))
-    #
-    #
-    #from sklearn.model_selection import train_test_split
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    #	#Naive Bayes Classifier
-    #from sklearn.naive_bayes import MultinomialNB
-    #
-    clf = MultinomialNB()
-    #clf.fit(X_train,y_train)
-    #clf.score(X_test,y_test)
-    #filename = 'nlp_model.pkl'
-    #pickle.dump(clf, open(filename, 'wb'))
+cv = CountVectorizer()
 
-    # Alternative Usage of Saved Model
-    # joblib.dump(clf, 'NB_spam_model.pkl')
-    # NB_spam_model = open('NB_spam_model.pkl','rb')
-    # clf = joblib.load(NB_spam_model)
 
-    if request.method == 'POST':
-        message = request.form['Text']
-        data = [Text]
-        vect = cv.transform(data).toarray()
-        my_prediction = clf.predict(vect)
-    return render_template('result.html', prediction=my_prediction)
+def predict(a):
+    cd = clean_data(a)
+    print(cd)
+    # cd = vect.transform(cd)
+    # print(cd)
+    # prediction = model.predict(cd)
+    return "prediction"
+
+
+def main():
+    st.title("""
+    **NEWS CLASSIFIER APP**
+    """)
+
+    html_temp = """
+    <div style= "background-color:#825246; padding:10px>
+    <h2 style = "color: white; text-align:center ;">Fake News detector ML app</h2>
+    </div>
+    """
+
+    st.markdown(html_temp, unsafe_allow_html=True)
+    st.subheader("Enter the news article you are not sure about")
+    user_input = st.text_input("Your news here below", "#SAMPLE TEXT#")
+
+    st.subheader("cleaned and preprocessed data")
+    st.write(clean_data(user_input))
+    if st.button("classify"):
+        output = predict(user_input)
+        if output == 0:
+            st.success('The above news is Fake')
+        else:
+            st.success('This is a real news')
+
+    else:
+        st.write("No input")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
